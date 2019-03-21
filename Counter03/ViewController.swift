7811dbcb102fd211e98864d0f4ee7271c83fe7fd
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-03-21 12:18:40 +0900

    count increased and the number is 9 than number changed 0

--- Counter03/ViewController.swift
+++ Counter03/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    var cnt = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myLabel.text = "0"
     }
-
+    
+    @IBAction func btAction(_ sender: Any) {
+        if cnt<9 {
+            cnt += 1
+            myLabel.text = String(cnt)
+        }
+        else{
+            cnt = 0
+            myLabel.text = String(cnt)
+        }
+    }
 
 }
 
